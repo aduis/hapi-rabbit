69e1ff3f8e2cb6ea30e07e1d5d700a70ad0d28f4
Andre Duis <andre.duis@gmail.com>
2014-12-14 18:24:48 +0100

    add tests and code coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,44 @@
-module.exports = function(grunt)
-{
+module.exports = function (grunt) {
+
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
 
     grunt.initConfig({
 
         mochaTest: {
             test: {
                 options: {
-                    reporter: 'spec',
-                    quiet: false,
-                    clearRequireCache: false 
+                    reporter         : 'spec',
+                    quiet            : false,
+                    clearRequireCache: false
                 },
-                src: ['test/**/*.js']
+                src    : ['test/**/*.js']
+            }
+        },
+
+        watch: {
+            scripts: {
+                files  : ['./lib/**/*.js', './test/**/*.js'],
+                tasks  : ['mochaTest', 'mocha_istanbul:coverage'],
+                options: {
+                    debounceDelay: 250
+                }
+            }
+        },
+
+        mocha_istanbul: {
+            coverage : {
+                src    : 'test',
+                options: {
+                    mask: '*.js'
+                }
             }
         }
 
     });
 
     grunt.registerTask('test', 'mochaTest');
+    grunt.registerTask('coverage', 'mocha_istanbul:coverage');
 
 };
--- test/specs.js
+++ test/specs.js
@@ -2,76 +2,202 @@ var should = require('chai').should();
 var Hapi = require("hapi");
 var hapiRabbit = require('../lib');
 
-describe('hapiRabbit', function(){
+var setup = function (callback) {
 
-  describe('plugin', function(){
-  	var server = null;
-  	var rabbit = null;
+    var server = new Hapi.Server();
+    server.register({
+        register: hapiRabbit
+    }, function (err) {
 
-  	beforeEach(function(done){
-  		server = new Hapi.Server();
-  		server.register({
-		    register: hapiRabbit
-		}, function (err) {
+        if (err) {
+            console.log('Failed loading plugin');
+        }
 
-		    if (err) {
-		        console.log('Failed loading plugin');
-		    }
+        var rabbit = server.plugins['hapi-rabbit'];
 
-			rabbit = server.plugins['hapi-rabbit'];
-		    done();
-		});
-		
-	});
-    
-    it('should be defined', function(){
-    	rabbit.should.be.an('object');
+        var result = {
+            rabbit: rabbit,
+            server: server
+        };
+
+        callback(null, result);
     });
 
-    it('should have subscribe function', function(){
-    	rabbit.should.respondTo('subscribe');
-    });
+};
 
-    it('should have publish function', function(){
-    	rabbit.should.respondTo('publish');
-    });
+var context = {
+    socket: function (type) {
+        var connect = false;
 
-    it('should have createContext function', function(){
-    	rabbit.should.respondTo('createContext');
-    });
+        if (type === 'SUB') {
+            connect = function (exchange, messageType, callback) {
+                callback();
+            }
+        } else if (type === 'PUB') {
+            connect = function (exchange, callback) {
+                callback();
+            }
+        }
 
-  });
+        return {
+            connect    : connect,
+            publish    : function () {
+            },
+            setEncoding: function () {
+            },
+            on         : function (event, callback) {
+                callback('{"type":"messageType","data":"message"}');
+            }
+        };
+    }
+};
 
-  describe('subscribe', function(){
-  	var server = null;
-  	var rabbit = null;
-  	var error = null;
+describe('hapiRabbit', function () {
 
-  	beforeEach(function(done){
-  		var context = {};
+    describe('plugin', function () {
+        var server = null;
+        var rabbit = null;
 
-  		server = new Hapi.Server();
-  		server.register({
-		    register: hapiRabbit
-		}, function (err) {
-
-		    if (err) {
-		        console.log('Failed loading plugin');
-		    }
-
-			rabbit = server.plugins['hapi-rabbit'];
-            rabbit.subscribe(context, 'exchange.bla', function(err, message){
-            	error = err;
+        beforeEach(function (done) {
+            setup(function (err, result) {
+                server = result.server;
+                rabbit = result.rabbit;
                 done();
             });
-		});
-		
-	});
-    
-    it('should return validation error when entering non-alphanumeric exchange', function(){
-    	error.name.should.equal('ValidationError');
+        });
+
+        it('should be defined', function () {
+            rabbit.should.be.an('object');
+        });
+
+        it('should have subscribe function', function () {
+            rabbit.should.respondTo('subscribe');
+        });
+
+        it('should have publish function', function () {
+            rabbit.should.respondTo('publish');
+        });
+
+        it('should have createContext function', function () {
+            rabbit.should.respondTo('createContext');
+        });
+
     });
 
-  });
+    describe('subscribe invalid exchange', function () {
+        var error = null;
+
+        beforeEach(function (done) {
+
+            setup(function (err, result) {
+                result.rabbit.subscribe(context, 'exchange.bla', function (err, message) {
+                    error = err;
+                    done();
+                });
+            });
+
+        });
+
+        it('should return validation error when entering non-alphanumeric value', function () {
+            error.name.should.equal('ValidationError');
+        });
+
+    });
+
+    describe('publish invalid exchange', function () {
+        var error = null;
+
+        beforeEach(function (done) {
+
+            setup(function (err, result) {
+                result.rabbit.publish(context, 'exchange.bla', 'messageType', 'message', function (err, message) {
+                    error = err;
+                    done();
+                });
+            });
+
+        });
+
+        it('should return validation error when entering non-alphanumeric value', function () {
+            error.name.should.equal('ValidationError');
+        });
+
+    });
+
+    describe('publish invalid messageType', function () {
+        var error = null;
+
+        beforeEach(function (done) {
+
+            setup(function (err, result) {
+                result.rabbit.publish(context, 'exchange', 'messageType.sdks', 'message', function (err, message) {
+                    error = err;
+                    done();
+                });
+            });
+
+        });
+
+        it('should return validation error when entering non-alphanumeric value', function () {
+            error.name.should.equal('ValidationError');
+        });
+
+    });
+
+    describe('publish', function () {
+        var msg = null;
+
+        beforeEach(function (done) {
+
+            setup(function (err, result) {
+                result.rabbit.publish(context, 'exchange', 'messageType', 'message', function (err, message) {
+                    msg = message;
+                    done();
+                });
+            });
+
+        });
+
+        it('should return messageObject', function () {
+            msg.should.be.an('object');
+        });
+
+        it('should return messageObject with messageType', function () {
+            msg.type.should.equal('messageType');
+        });
+
+        it('should return messageObject with data', function () {
+            msg.data.should.equal('message');
+        });
+
+    });
+
+    describe('subscribe', function () {
+        var msg = null;
+
+        beforeEach(function (done) {
+
+            setup(function (err, result) {
+                result.rabbit.subscribe(context, 'exchange', function (err, message) {
+                    msg = message;
+                    done();
+                });
+            });
+
+        });
+
+        it('should return messageObject', function () {
+            msg.should.be.an('object');
+        });
+
+        it('should return messageObject with messageType', function () {
+            msg.type.should.equal('messageType');
+        });
+
+        it('should return messageObject with data', function () {
+            msg.data.should.equal('message');
+        });
+
+    });
 
 });
